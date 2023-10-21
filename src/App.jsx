b25fde6e32d8a273e28c2592b251e536ba8fed13
Leon Munene <106140292+Leon8M@users.noreply.github.com>
2023-10-21 11:37:12 +0000

    educ

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
+import Education from "./Components/Education";
 
 import "./styles.css";
 
@@ -35,7 +36,7 @@ const siteProps = {
   youTube: "",
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "#0F0509";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
@@ -45,6 +46,7 @@ const App = () => {
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
       <Portfolio />
+      <Education />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
